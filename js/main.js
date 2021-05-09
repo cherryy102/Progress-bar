f2acd8c69bf31ec8053f77f9c58c8b0bd9747f15
Damian Wiśniowski <dkontow@gmail.com>
2021-05-09 12:41:39 +0200

    end progress-bar

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const barLine = document.querySelectorAll('.progress__bar__line');
+const barPercent = document.querySelectorAll('.progress__bar__percent ');
+
+const progressBar = (percent, time, barID) => {
+    const intervalTime = (time * 1000) / percent;
+    let addPercent = 0;
+    const barInterval = setInterval(() => {
+        addPercent++;
+        barLine[barID].style.width = `${addPercent}%`;
+        barPercent[barID].style.width = `${addPercent}%`;
+        barPercent[barID].textContent = `${addPercent}%`;
+        if (addPercent === percent) {
+            clearInterval(barInterval);
+        }
+    }, intervalTime);
+}
+progressBar(69, 1.5, 0);
